024f6c612f6660c351092bede2887e9395c3ed0d
Jawad Afzaal <jawad.afzaal94@gmail.com>
2025-05-15 15:27:33 +0800

    contact.sql
    FP Test t2

---
+++ contact.sql
@@ -0,0 +1,11 @@
+-- contact
+
+CREATE TABLE contact (
+Order_ID int,
+contact_time DATETIME
+)
+INSERT INTO Contact (Order_id, contact_time)
+VALUES (1, '2019-12-24 00:25:00'),
+(2, '2019-12-24 15:30:00'),
+(4, '2019-12-24 19:38:00'),
+(5, '2019-01-28 19:46:00');
