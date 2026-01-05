7cd9a2febfe040f2b3a5e08eab72cb2e675ebde2
Jawad Afzaal <jawad.afzaal94@gmail.com>
2026-01-06 01:11:19 +0500

    Update SQLQuery1 - 2025-01-04.sql

--- SQLQuery1 - 2025-01-04.sql
+++ SQLQuery1 - 2025-01-04.sql
@@ -1,31 +1,41 @@
 -- DISCTINCT = UNIQUE
-SELECT DISTINCT MAKE FROM OLX_CARS
+SELECT 
+	DISTINCT MAKE 
+FROM OLX_CARS
 
 -- MAKE, MODEL
-SELECT DISTINCT MAKE, MODEL FROM OLX_CARS
+SELECT 
+	DISTINCT MAKE, 
+	         MODEL 
+FROM OLX_CARS
 
-SELECT DISTINCT MAKE, MODEL 
+SELECT 
+	DISTINCT MAKE, 
+	         MODEL 
 FROM OLX_CARS
 ORDER BY MAKE, MODEL
 
-SELECT DISTINCT MODEL, MAKE
+SELECT 
+	DISTINCT MODEL, 
+	          MAKE
 FROM OLX_CARS
 ORDER BY MODEL, MAKE
 
-SELECT * FROM OLX_CARS WHERE DESCRIPTION IS NULL
+SELECT * 
+FROM OLX_CARS 
+WHERE DESCRIPTION IS NULL
 
-SELECT * FROM GlobalSharkAttack
-where area is null
+SELECT * 
+FROM GlobalSharkAttack
+WHERE area IS NULL
 
 -- country, area, location
 
-SELECT * FROM GlobalSharkAttack
-WHERE
-AREA IS NULL
-AND
-LOCATION IS NULL
-AND
-COUNTRY IS NULL
+SELECT * 
+FROM GlobalSharkAttack
+WHERE AREA IS NULL
+  AND LOCATION IS NULL
+  AND COUNTRY IS NULL
 
 -- LIKE
 -- wo sarey log jo samandar me nahatey huey
@@ -34,21 +44,26 @@ COUNTRY IS NULL
 
 SELECT *
 FROM GlobalSharkAttack 
-where activity like '%bath%'
+WHERE activity LIKE '%bath%'
 
 -- fatal
--- like fatal
+-- LIKE fatal
 
-select * from FinalGlobalSharkAttack
-where Activity like 'Bath%' and Injury like 'Fatal%'
+SELECT * 
+FROM FinalGlobalSharkAttack
+WHERE Activity LIKE 'Bath%' 
+AND Injury LIKE 'Fatal%'
 
 -- show tables
 
-select * from INFORMATION_SCHEMA.TABLES
+SELECT * 
+FROM INFORMATION_SCHEMA.TABLES
 
-select * from sys.tables
+SELECT * 
+FROM sys.tables
 
-select * from city
+SELECT * 
+FROM city
 
 CREATE TABLE participants
 (FirstName varchar(255),
@@ -56,37 +71,44 @@ LastName varchar(255))
 
 -- participants (FirstName, LastName)
 
-select * from participants
+SELECT * 
+FROM participants
 
 INSERT INTO participants
 VALUES
 ('Noor', 'Surani')
 
 
-SELECT * FROM participants
+SELECT * 
+FROM participants
 WHERE FirstName LIKE 'A%';
 
 
-select * from participants  
-where firstname like 'A%' or lastname like 'A%'
+SELECT * 
+FROM participants  
+WHERE firstname LIKE 'A%' or lastname LIKE 'A%'
 
-select * from participants
-where lastname like 'SU%'
+SELECT * 
+FROM participants
+WHERE lastname LIKE 'SU%'
 
-select * from participants
-where FirstName like 'N%' -- % wildcard
+SELECT * 
+FROM participants
+WHERE FirstName LIKE 'N%' -- % wildcard
 
-select * from participants
-where FirstName = 'Jawad'
+SELECT * 
+FROM participants
+WHERE FirstName = 'Jawad'
 
 ALTER TABLE participants
 ADD Marks int
 
 -- Power of Now
 
-select * from participants
-where marks is not null
-order by marks desc
+SELECT * 
+FROM participants
+WHERE marks is not null
+ORDER BY marks DESC
 
 UPDATE participants
 SET Marks = 75
@@ -96,10 +118,11 @@ AND LastName = 'Ansari'
 ALTER TABLE participants
 ADD ParticipantId INT IDENTITY(1,1);
 
-SELECT * FROM participants
+SELECT * 
+FROM participants
 WHERE FirstName = 'Noor'
-AND
-LastName = 'Surani';
+  AND LastName = 'Surani';
+
 WHERE marks = 100
 
 INSERT INTO participants
@@ -117,9 +140,9 @@ VALUES
 
 UPDATE participants
 SET Marks = 75
-WHERE FirstName like 'Jawad'
+WHERE FirstName LIKE 'Jawad'
 and
-LastName like 'Afzaal'
+LastName LIKE 'Afzaal'
 
 
 -- 1, +1
@@ -132,66 +155,88 @@ WHERE FirstName = 'Noor'
 AND
 LastName = 'Surani';
 
-select * From participants
-where
-FirstName like 'asif'
+SELECT * 
+FROM participants
+WHERE FirstName LIKE 'asif'
 
-select * from supermarket_sales
+SELECT * 
+FROM supermarket_sales
 
-SELECT SUM(QUANTITY) FROM supermarket_sales
+SELECT 
+ 	SUM(QUANTITY) 
+FROM supermarket_sales
+
+	
 -- COUNT
 -- MAX
 -- MIN
-SELECT SUM(Quantity) AS QTY_SUM
+
+	
+SELECT 
+	SUM(Quantity) AS QTY_SUM
 FROM SUPERMARKET_SALES
 
-SELECT SUM(quantity)
+SELECT 
+	SUM(quantity)
 FROM supermarket_sales;
 
-SELECT COUNT(*) AS TotalSales
+SELECT 
+	COUNT(*) AS TotalSales
 FROM supermarket_sales;
 
-SELECT COUNT(quantity) AS QuantityCount
+SELECT 
+	COUNT(quantity) AS QuantityCount
 FROM supermarket_sales;
 
-SELECT MIN(quantity) AS MinQuantity
+SELECT 
+	MIN(quantity) AS MinQuantity
 FROM supermarket_sales;
 
-SELECT MAX(quantity) AS MaxQuantity
+SELECT 
+	MAX(quantity) AS MaxQuantity
 FROM supermarket_sales;
 
-SELECT AVG(quantity) AS MaxQuantity
+SELECT 
+	AVG(quantity) AS MaxQuantity
 FROM supermarket_sales;
 
 
 
 -- AS (Alias)
 
-select * from participants
+SELECT * 
+FROM participants
 
-select FirstName AS first,
-LastName as last from participants
-
-
-select firstname + ' ' + lastname as FullName
-from participants
+SELECT 
+	FirstName AS first,
+LastName as last FROM participants
 
 
-select city, count(*) as RecCount,
-sum(quantity) as SumQuantity,
-max(quantity) as MaxQuantity,
-min(quantity) as MinQuantity,
-avg(quantity) as AvgQuantity
-from supermarket_sales
-group by city
+SELECT firstname + ' ' + lastname as FullName
+FROM participants
 
-select * from supermarket_sales
 
-select * from OLX_CARS
+SELECT 
+	city, count(*) as RecCount,
+	sum(quantity) as SumQuantity,
+	max(quantity) as MaxQuantity,
+	min(quantity) as MinQuantity,
+	avg(quantity) as AvgQuantity
+FROM supermarket_sales
+GROUP BY city
+
+SELECT * 
+FROM supermarket_sales
+
+SELECT * 
+FROM OLX_CARS
 
 -- Make, Model count
 
-SELECT MAKE, MODEL, COUNT(*)
+SELECT 
+	MAKE, 
+	MODEL, 
+	COUNT(*)
 FROM OLX_CARS
 GROUP BY MAKE, MODEL
 
@@ -203,36 +248,51 @@ FROM OLX_CARS
 GROUP BY Make, Model
 ORDER BY Make ASC, TotalCars DESC;
 
-SELECT OC.MAKE, OC.MODEL, COUNT(*) as "Total Records"
+SELECT 
+	OC.MAKE,
+	OC.MODEL, 
+	COUNT(*) as "Total Records"
 FROM OLX_CARS OC
 GROUP BY oc.MAKE, oc.MODEL
-order by oc.make desc, oc.model asc
+ORDER BY oc.make DESC, oc.model asc
 
-select * From GlobalSharkAttack
--- Country, desc count(*)
+SELECT * 
+FROM GlobalSharkAttack
 
--- group by country
+	
+-- Country, DESC count(*)
+
+-- GROUP BY country
 -- display country, count(*) as incidentcount
--- order country, incident desc
-select Country,count(*) as incident from GlobalSharkAttack
-Group by country 
-having count(*) > 100
-Order by incident desc
+-- order country, incident DESC
 
-select * from sys.tables
-where name like '%pak%'
+	
+SELECT Country,
+	count(*) as incident 
+FROM GlobalSharkAttack
+GROUP BY country 
+HAVING COUNT(*) > 100
+ORDER BY incident DESC
 
-select * from GlobalSharkAttack
+SELECT * 
+FROM sys.tables
+WHERE name LIKE '%pak%'
 
-Select country, Count(*) as incident
-	From GlobalSharkAttack
-	group by country
-	having count(*) > 100
-	ORDER BY incident DESC;
+SELECT * 
+FROM GlobalSharkAttack
+
+SELECT 
+	country, 
+	Count(*) as incident
+FROM GlobalSharkAttack
+GROUP BY country
+HAVING COUNT(*) > 100
+ORDER BY incident DESC;
 
 -- ye ishq nahi asaan
 
-select * from aah_pakelection
+SELECT * 
+FROM aah_pakelection
 
 -- year wise votes, valid votes sum
 -- order year
@@ -249,30 +309,44 @@ ORDER BY YEAR ASC
 
 -- FORMAT(NUMERIC FUNCTION OR VALUE, 'N0') AS NAME
 
-select party, year, sum(votes) AS TotalVotes
-from aah_pakelection
-group by party, year
-order by year, TotalVotes desc
-
-Select party, year ,\Format(Sum(Votes),'N0') As F_Votes
-From aah_pakelection
-Where Year Is NOT NULL
-Group by Year
-Order by year ASC
-
-
-SELECT party, year, format(sum(votes),'N0') as sumvotes
+SELECT 
+	party, 
+	year, 
+	sum(votes) AS TotalVotes
 FROM aah_pakelection
-where year is not null
-group by party, year
-order by year, sumvotes desc
+GROUP BY party, year
+ORDER BY year, TotalVotes DESC
 
-select * from tblEmployee
-select * from tblDesignation
+SELECT 
+	party, 
+	year ,
+	\Format(Sum(Votes),'N0') As F_Votes
+FROM aah_pakelection
+WHERE Year Is NOT NULL
+GROUP BY Year
+ORDER BY year ASC
 
-select e.*, d.*
-	from tblEmployee e
-		inner join tblDesignation d
-			on e.DesignationID=d.DesignationID
 
-select * from sys.tables
+SELECT 
+	party, 
+	year, 
+	format(sum(votes),'N0') as sumvotes
+FROM aah_pakelection
+WHERE year is not null
+GROUP BY party, year
+ORDER BY year, sumvotes DESC
+
+SELECT * 
+FROM tblEmployee
+	
+SELECT * 
+FROM tblDesignation
+
+SELECT e.*, d.*
+FROM tblEmployee e
+INNER JOIN tblDesignation d
+		ON e.DesignationID=d.DesignationID
+
+SELECT * 
+FROM sys.tables
+
