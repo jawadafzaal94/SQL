2273dee005ecdd8ca1c00940d4e9388af20fc8e1
Jawad Afzaal <jawad.afzaal94@gmail.com>
2026-03-10 02:05:37 +0500

    Implement latest status retrieval with CTE
    Added a CTE to retrieve the latest order status before contact.

--- test_fp.sql
+++ test_fp.sql
@@ -22,3 +22,34 @@ SELECT
 
 FROM LatestStatus LS
 WHERE LS.RN = 1;
+
+
+-------------- CHECK BELOW ------------
+
+WITH latest_status AS (
+
+      SELECT 
+          Order_Status.Order_ID AS order_id,
+          Order_Status.order_status_id AS order_status_id,
+          Order_Status.Timestamp AS timestamp,
+          Contact.Contact_time AS contact_time,
+          meta_status.status_description AS status_description,
+          ROW_NUMBER() OVER (PARTITION BY Order_Status.Order_ID, Contact.Contact_time
+                                 ORDER BY Order_Status.Timestamp DESC) AS rnk
+       FROM Order_Status
+       JOIN Contact
+         ON Order_Status.Order_ID = Contact.Order_id
+        AND Order_Status.Timestamp < Contact.Contact_time
+  LEFT JOIN meta_status
+         ON Order_Status.order_status_id = meta_status.order_status_id
+  
+)
+
+      SELECT 
+          order_id,
+          order_status_id,
+          timestamp,
+          contact_time,
+          status_description
+      FROM latest_status
+      WHERE rnk = 1
